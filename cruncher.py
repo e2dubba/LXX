f6fad18366f057b15d721420ca134af6ad0d00b5
e2dubba <svirgilgoodwin@gmail.com>
2016-09-26 08:50:11 -0500

    add test file

---
+++ cruncher.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess as sp
+
+def cruncher(form):
+    poss_roots = sp.run(['cruncher'], input=form, stdout=sp.PIPE, 
+            universal_newlines=True, env=os.environ).stdout
+    poss_roots = poss_roots.split('\n')
+    try: 
+        poss_roots = poss_roots[1].replace('<NL>', '').split('</NL>')
+    except IndexError:
+        print('Error! ' + form + '\n')
+    parsing_list = []
+    for item in poss_roots:
+        templist = item[2:].split(maxsplit=1)
+        temptup = (templist[0],
+                templist[1].split('\t', maxsplit=1)[0])
+        parsing_list.append(temptup)
+
+    return parsing_list
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('form', nargs='*')
+    args = parser.parse_args()
+    key = args.form
+    print(cruncher(key))
+    
